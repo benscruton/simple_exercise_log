d77188ce845705c3c8e463468dd400a83eb10356
benscruton <benjamin.scruton@gmail.com>
2021-10-08 15:08:00 -0500

    rendering a simple template

--- log_app/views.py
+++ log_app/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("hello")
+    return render(request, "index.html")
