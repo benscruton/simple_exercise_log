4dd2e74f02860d14adbac2a6cf20d593271ad60c
benscruton <benjamin.scruton@gmail.com>
2021-10-15 17:34:30 -0500

    Added model

--- log_app/models.py
+++ log_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Workout(models.Model):
+    type_of_exercise = models.CharField(max_length=255)
+    duration = models.IntegerField()
+    date = models.DateField()
+    notes = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
